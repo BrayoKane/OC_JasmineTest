49a5615edcfc970df5e488fb34aba0f6f7e94274
BrayoKane <brianoti44@gmail.com>
2019-10-12 23:00:42 +0300

    perf(createCandyCounter) implement

--- spec/createCandyCounterSpec.js
+++ spec/createCandyCounterSpec.js
@@ -1,7 +1,24 @@
-describe("candyCounter", function() {
+describe("CandyQuantity", function () {
+    let calc;
 
-    it("should subtract two numbers together", function () {
-        expect(true).toBe(false);
+    beforeEach(function () {
+        calc = new CandyQuantity();
+    });
+
+    describe("when calc is used to tell the quantity of candies available", function () {
+
+        it("should be able to calculate the quantity of Gummy bear candies left", function () {
+            expect(calc.subtract(510, 20)).toEqual(490);
+        });
+
+        it("should properly tell the total number of Runts candies available", function () {
+            expect(calc.sum(4, 600)).toEqual(604);
+        });
+
+        it("should properly tell that the reduction in one candy's type quantity does not affect the other", function () {
+            expect(calc.notSame(490, 604)).toEqual(true);
+        });
+
     });
 
 });
--- src/createCandyCounter.js
+++ src/createCandyCounter.js
@@ -1,3 +1,14 @@
-function add(x, y) {
-    return x + y;
-}
+CandyQuantity = function () {
+};
+
+CandyQuantity.prototype.subtract = function (GummyBear1, GummyBear2) {
+    return GummyBear1 - GummyBear2;
+};
+
+CandyQuantity.prototype.sum = function (Runt1, Runt2) {
+    return Runt1 + Runt2;
+};
+
+CandyQuantity.prototype.notSame = function (GummyBearQuantity, RuntQuantity) {
+    return GummyBearQuantity !== RuntQuantity;
+};
